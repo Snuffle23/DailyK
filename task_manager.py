317f114ce239647d8aaa25d80b437ed3dfdaa451
ASUS TUF A15 <matvelichko@gmail.com>
2025-12-16 16:11:47 +0700

    Удалено: Цикл запуска, task_manager() Добавлено: Задачи теперь хранятся в json, имеют дату установки, крайний срок и описание.

--- task_manager.py
+++ task_manager.py
@@ -1,32 +1,37 @@
-"""Повестка дня
-Удалить повторы \n 
-Придумать реализацию сроков"""
+import json
+import datetime
 
+def get_tasks(file):
+    """Возвращает словарь с задачами
+    file - путь к файлу с задачами"""
 
-nav_panel = """[1] - Новая задача
-[2] - Ближайшие задачи
-[3] - Все задачи"""
+    #Проверить на существование 
 
+    with open(file, "r", encoding="UTF-8") as to_do_list:
+        tasks = json.load(to_do_list)
+    return tasks
 
-def task_manager(n):
-    """Принимает на вход число"""
-    file = open("tasks.txt", "+a", encoding="UTF-8")
+def create_task(tasks:dict={}): #реализовать множественное создание задач
+    """Добавляет элемент(задачу) формата:
+    "newTask":{"deadline":"dd-mm-yy","note":"description", "set":"dd-mm-yy"} 
+    в указанный словарь
+    task_list - указанный словарь""" 
 
-    if n == 1: #Добавляет задачу в файл tasks.txt
-        tasks = file.read()
-        while (text := input()) != "":
-            tasks += "\n" + text
-        file.write(tasks)        
-    elif n == 2:
-        pass
-    elif n == 3: # Выводит в консоль список всех задач
-        file.seek(0)
-        print(file.read())
-    
-    file.close()
+    set = (datetime.date.today()).strftime("%d-%m-%y")
+    task = input("task name: ")
+    if tasks.get(task, "") == "":#проверка на существование
+        deadline = input("deadline dd-mm-yy: ")
+        note = (s if (s:=input()) != "" else "")
+        print(f"task \'{task}\' created.")
+        tasks[task] = {"deadline":deadline, "note":note, "set":set}
+    else:
+        print(f"task \'{task}\' already exist.")
 
+def upd_json(file, content:dict):
+    """Записывает данные content в file(json)
+    file - путь до файла
+    content - запысываемые данные"""
+    with open(file, 'w', encoding="UTF-8") as file_out:
+        json.dump(content, file_out, ensure_ascii=False, indent=2)
 
-print(nav_panel)
-while (cur_action := int(input("Ввод: "))) > 0:
-    task_manager(cur_action)
 
