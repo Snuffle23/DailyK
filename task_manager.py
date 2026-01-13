5c73703cd65c92ebd9820a3f4c99b9711be598a5
ASUS TUF A15 <matvelichko@gmail.com>
2026-01-14 00:25:11 +0700

    Польностью изменена функция создания задач и немного изменена структура задач, добавлен строгий фильтр по значению аргумента и функция отладки.

--- task_manager.py
+++ task_manager.py
@@ -5,9 +5,9 @@ import datetime
 def get_json(file):
     """Возвращает словарь
 
-    :param file: путь к файлу
+    :param file: путь к файлу(можно не указывать формат файла, потому что функция только для json)
     """
-
+    file = file + ".json" if not file.endswith(".json") else file
     with open(file, "r", encoding="UTF-8") as file_in:
         result = json.load(file_in)
     return result
@@ -15,25 +15,78 @@ def get_json(file):
 def upd_json(file, content:dict):
     """Записывает данные content в file(json)
 
-    :param file: путь до файла
+    :param file: путь до файла(можно не указывать формат файла, потому что функция только для json)
     :param content: запысываемый словарь"""
+
+    file = file + ".json" if not file.endswith(".json") else file
     with open(file, 'w', encoding="UTF-8") as file_out:
         json.dump(content, file_out, ensure_ascii=False, indent=4)
 
+def name_normalize(name:str="", dict={}):
+        """Возвращает нормализированную строку (без пробелов в начале и в конце), добавляет _num, чтобы получить строку, подходящую как ключ для словаря dict"""
+        
+        while name.endswith(" "):#срез строки без последнего символа, пока кончается на пробел
+            name = name[0:-1]
+        while name.startswith(" "):#срез строки без первого символа, пока начинается на пробел
+            name = name[1::]
 
-def name_normalize(name:str=""):
-    """Возвращает нормализированную строку (без пробелов в начале и в конце)
-    
-    :param name: Строка 
+        while name in dict.keys():
+            elems = name.split("_")
+            digit_end = f"_{int(elems[-1]) + 1}" if (elems[-1].isdigit()) else "_1"
+            name = ("_".join(elems[:-1:] if len(elems) > 1 else elems) )+ digit_end
+        return name
+
+
+def tasking(estime:int=1, deadline:str=False, note:str=False, difficult:str=False, components = False, main:bool=True):
+    """Возвращает словарь с данными задачи
+
+    :param estime: Описывает время в минутах, требующееся для выполнения задачи. 
+    Из него расчитывается сложность задачи.
+    :param deadline: Крайний срок задачи. Не может быть прошедшей датой.
+    :param note: Описание задачи (необязательное)
+    :param difficult: Задаёт сложность задачи. Используется в тех случаях, когда тип задачи не соответсвует расчетному времени.
+    :param components: Словарь подзадач, будет использован при создании сложной задачи.
+    :param main: Определяет структуру заполнения выходного словаря. True для основных задач False для подзадач.
     """
-    while name != "":#Генерация и проверка имени
-        if name.startswith(" ") or name.endswith(" "):
-            while name.endswith(" "):
-                name = name[0:-2]
-            while name.startswith(" "):
-                name = name[1::]
-        break
-    return name
+
+    def sub_tasking():
+        while (sub_name:= input(f"Имя позадачи {len(components.keys()) + 1}: ")) != "":
+            while (sub_estime:=input("Расчетное время: ")) != "":
+                if sub_estime.isdigit():
+                    if int(sub_estime) > hard:
+                        print("Время выходит за диапозон. Укажите меньше 120 минут")
+                    else:
+                        break
+                else:
+                    print("Введите число")
+            sub_note = input("Описание: ")
+            components[name_normalize(sub_name, components)] = tasking(int(sub_estime), note=sub_note, main=False)
+        if len(components.keys()) < 1:
+            sub_tasking()
+
+    set = (datetime.date.today()).strftime("%d.%m.20%y")
+    components = {} if not components else components
+
+    easy = 30 # 30 минут
+    midl = 60 # час
+    hard = 240 # 4 часа
+    if estime <= easy: 
+        diff = "easy"
+    elif estime <= midl:
+        diff = "midl"
+    elif estime <= hard:
+        diff = "hard"
+    elif estime > hard or difficult == "complex":
+        diff = "complex"
+        print(f"У комплексной задачи должны быть подзадачи")
+        sub_tasking()
+    
+    if main and components:
+        return {"difficult":diff, "deadline":deadline, "estime":estime, "note":note, "components":components, "set":set, "progress":0.0, "complete":0}
+    elif main:
+        return {"difficult":diff, "deadline":deadline, "estime":estime, "note":note, "set":set, "complete":0}
+    else:
+        return {"difficult":diff, "estime":estime, "note":note, "complete":0}
 
 def overdue_checkup(tasks:dict, overdue=False, arg="deadline"):
     """
@@ -74,40 +127,6 @@ def arg_checkup(tasks:dict, arg:str, value=""):
                 final_list.append(task_name)
     return final_list
 
-def create_task(tasks:dict={}): #реализовать множественное создание задач
-    """Добавляет задачу в указанный словарь""" 
-    name = name_normalize(input("Название: "))
-    while not (name not in tasks.keys()) and (name != ""):
-        name = name_normalize(input("Попробуйте другое: "))
-    if not name:
-        return
-    #Генерация контента
-    set = (datetime.date.today()).strftime("%d.%m.%y")
-    deadline = input("Дедлайн: ")
-    note = input("Описание: ")
-    difficult = {1:"easy", 2:"midl", 3:"hard", 4:"comp"}[int(num)] if ((num:=input("Сложность 1-4: ")) < "5") and num.isdigit() else 1
-    if difficult == "comp":
-        components = {}
-        while (subtask := input("Подзадача: ")) != "":
-            subtask = name_normalize(subtask)
-            while subtask in components.keys() and (subtask != ""):
-                subtask = name_normalize(input("Попробуйте другое: "))
-            sub_difficult = {1:"easy", 2:"midl", 3:"hard"}[int(num)] if ((num:=input("Сложность 1-3: ")) < "4") and num.isdigit() else 1
-            components[subtask] = {"note": input("Описание: "), "difficult":sub_difficult, "complete":0}
-        tasks[name] = {
-            "deadline": deadline,
-            "note": note,
-            "difficult": difficult,
-            "components":components,
-            "set": set, "complete": 0}
-        return
-    
-    tasks[name] = {
-        "deadline": deadline,
-        "note": note,
-        "difficult": difficult,
-        "set": set, "complete": 0}
-    return
 
 def bubble_sort(list):
     """Возвращает список, отсортированый методом пузырька"""
--- testing.py
+++ testing.py
@@ -1,17 +1,51 @@
 import json, datetime
 from task_manager import *
 
-tasks = get_json("tasks.json")
+
+
+
 def task_filter():
     pass
 
 
-def tasking():
-    pass
 
-name = ""
-deadline = ""
-note = ""
-difficult = ""
-set = ""
-complete = 0
+def dtools(choise:int=1):
+    """
+    Docstring для etool
+    
+    :param choise: Выбор номера 
+    :type choise: int
+    :param tasks: Доп параметры для функций
+    """
+    
+    def show_tasks():
+        for i, task in enumerate(tasks):
+            print(i+1, task)
+            for arg in tasks[task].keys():
+                if arg == "components":
+                    print(f"  components:")
+                    for sub_tusk in tasks[task][arg]:
+                        print(f"  - {sub_tusk}")
+                        for subArg in tasks[task][arg][sub_tusk]:
+                            print(f"      {subArg} - {tasks[task][arg][sub_tusk][subArg]}")
+                else:
+                    print(f"  {arg} - {tasks[task][arg]}")
+    
+    def add_task():
+        while (name := input("Имя задачи: ")) != "":
+            tasks = get_json("tasks")
+            name = name_normalize(name, tasks)
+            deadline = input("Дэдлайн: ")
+            estime = int(input("Расчётное время: "))
+            tasks[name] = tasking(estime, deadline)
+            upd_json("tasks", tasks)
+
+    if choise == 1:
+        show_tasks()
+    elif choise == 2:
+        add_task()
+
+
+
+dtools(2)
+
